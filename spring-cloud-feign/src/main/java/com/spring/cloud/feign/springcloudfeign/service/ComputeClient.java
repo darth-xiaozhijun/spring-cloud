3143ef8b8b23215d40132a9fff6f245efe65595b
xiaozhijun <15717061273@163.com>
2019-08-15 19:39:55 +0800

    feign实现负载均衡

---
+++ spring-cloud-feign/src/main/java/com/spring/cloud/feign/springcloudfeign/service/ComputeClient.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.feign.springcloudfeign.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("compute-client")
+public interface ComputeClient {
+
+    @RequestMapping(method = RequestMethod.GET,value = "/add")
+    Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
+}
